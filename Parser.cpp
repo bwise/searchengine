3e223c773192048a72ed6e69a618c3d0fe276314
KristoforHorst <kristoforh@gmail.com>
2014-11-18 13:29:11 -0600

    started parser, included sanity.exe (minesweeper), included perlscript for XML splitting

---
+++ Parser.cpp
@@ -0,0 +1,42 @@
+
+#include "Parser.h"
+
+Parser::ParseMain(char* fileName)
+{
+string readBuffer = "";
+string nextWord = "";
+string nextTag = "";
+string uniqueID = "";
+
+porter2_stemmer stemmer = new porter2_stemmer();
+
+//read words from the XML base file
+ iostream readInit();
+ readInit.open(fileName);
+ 
+ while(!readInit.eof())
+ {
+	//check if you get a tag
+	readInit >> readBuffer;
+	if (readBuffer[0] == '<') //you have a tag
+		nextTag = readBuffer; //TODO: clear < > 
+
+	//either create new tag, or append word to tag
+	checkTag( nextTag) //TODO: create a function that checks a tag array/ checks if we need the info
+
+	//check if you get a word or a uniqueID
+	 if(nextTag == "ID") //TODO: check XML tags for ID identifier
+		 {
+		 uniqueID = readBuffer; //get the unique ID associated with the word
+		 }
+	 else
+		 {
+		 nextWord = readBuffer; //TODO: check that this is a correct assumption
+		 //stem the word
+		nextWord = stemmer.stem(nextWord);
+		 //send the word and its ID to the dictionary
+		 dictonary.addWord(unqiueID, nextWord);
+		 } 
+
+	}
+}
---
+++ Parser.h
@@ -0,0 +1,18 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+
+#include <iostream>
+#include <fstream>
+#include "TinyXML.h"
+#include "porter2_stemmer.h"
+#include <string>
+
+class Parser
+{
+	Parser(){};
+	ParserMain(char*);
+
+}
+
+#endif
